9439d7a281accef4f3bf570a04e800517c84c935
Shashank <shashanksinghal02@gmail.com>
2023-11-24 00:57:38 +0530

    - Upgraded kotlin version

--- lib/utils/app_utils.dart
+++ lib/utils/app_utils.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -5,6 +6,10 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../models/signin_response_model.dart';
 import '../routes/app_routes.dart';
@@ -113,6 +118,61 @@ class AppUtils {
       return "GOOD AFTERNOON";
     } else {
       return "GOOD EVENING";
+    }
+  }
+
+  static Future<void> shareImageFromApi({
+    required String imageUrl,
+    required String text,
+    required String subject,
+  }) async {
+    // Download the image from the API link.
+    final response = await http.get(Uri.parse(imageUrl));
+
+    // Get the temporary directory on the device.
+    final tempDir = await getTemporaryDirectory();
+
+    // Create a file with a unique name in the temporary directory.
+    const tempFileName = 'image.png';
+    final tempFile = File('${tempDir.path}/$tempFileName');
+
+    // Write the downloaded image data to the temporary file.
+    await tempFile.writeAsBytes(response.bodyBytes);
+
+    // Share the temporary image file using Share.shareXFiles.
+    await Share.shareXFiles([XFile(tempFile.path)],
+        subject: subject, text: text);
+
+    // Delete the temporary image file.
+    await tempFile.delete();
+  }
+
+  static Future<void> downloadImage(String url, String fileName) async {
+    try {
+      // Download image
+      final http.Response response = await http.get(Uri.parse(url));
+
+      // Get temporary directory
+      final dir = await getTemporaryDirectory();
+
+      // Create an image name
+      var filename = '${dir.path}/$fileName';
+
+      // Save to filesystem
+      final file = File(filename);
+      await file.writeAsBytes(response.bodyBytes);
+
+      // Ask the user to save it
+      final params = SaveFileDialogParams(sourceFilePath: file.path);
+      final finalPath = await FlutterFileDialog.saveFile(params: params);
+
+      if (finalPath != null) {
+        showSnackBar("Image saved to disk",
+            title: "Success", status: MessageStatus.SUCCESS);
+      }
+    } catch (e) {
+      showSnackBar("An error occurred while saving the image",
+          title: "Error", status: MessageStatus.ERROR);
     }
   }
 }
